175e71020b3c13cc4377ead9316f8fd7308fbe9f
butterflysly53 <77926829+butterflysly53@users.noreply.github.com>
2022-01-13 18:52:58 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,19 +1,20 @@
 <?php
 //Development connection
- /*$host="localhost";
+ $host="localhost";
  $db="earthquakenew";
  $user="root";
  $pass="";
- $charset="utf8mb4";*/
+ $charset="utf8mb4";
 
 //Remote Database Connection
-
+/*
  $host="sql6.freesqldatabase.com";
- $db="sql6440167";
- $user="sql6440167";
- $pass="Wv5sUIDvEU";
+ $db="sql6446412";
+// $user="sql6440167";
+ $user="sql6446412";
+ $pass="4w6kszQcBk";
  $charset="utf8mb4";
- 
+ */
  $dsn="mysql:host=$host;dbname=$db;charset=$charset";
  
  try{
