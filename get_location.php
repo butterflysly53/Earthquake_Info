7272e10717d86f5fe4e8a2eab3226af03a15ad22
butterflysly53 <77926829+butterflysly53@users.noreply.github.com>
2022-01-13 18:52:45 +0530

    Update get_location.php

--- get_location.php
+++ get_location.php
@@ -1,6 +1,6 @@
 <?php
-    //$db = mysqli_connect('localhost','root','','earthquakenew')
-    $db = mysqli_connect('sql6.freesqldatabase.com','sql6440167','Wv5sUIDvEU','sql6440167')or die('Error connecting to MySQL server.');
+    $db = mysqli_connect('localhost','root','','earthquakenew');
+    //$db = mysqli_connect('sql6.freesqldatabase.com','sql6440167','Wv5sUIDvEU','sql6440167')or die('Error connecting to MySQL server.');
   $query = "SELECT* FROM places";
   $result = mysqli_query($db, $query) or die('Error querying database.');
   $myArray = array();
